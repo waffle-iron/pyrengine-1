cb51933cbd71aaa3b2316474341088189b5f9925
Johannes du Randt <jrdurandt@protonmail.com>
2017-08-19 16:38:42 +0200

    Added basic vulkan code

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,29 +1,81 @@
+import sys
+import logging
 import glfw
+import vulkan as vk
 
-def main():
-    # Initialize the library
-    if not glfw.init():
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+
+class Demo:
+    window = None
+    instance = None
+
+    def run(self):
+        logger.debug('Starting...')
+
+        self.init_window()
+        self.init_vulkan()
+        self.main_loop()
+        self.cleanup()
+
+    def init_window(self):
+        logger.debug('Init window')
+        if not glfw.init():
+            return 1
+
+        glfw.window_hint(glfw.CLIENT_API, glfw.NO_API)
+        glfw.window_hint(glfw.RESIZABLE, False)
+
+        self.window = glfw.create_window(800, 600, "Pyrengine", None, None)
+
+    def init_vulkan(self):
+        logger.debug('Init vulkan')
+        self.create_instance()
         return
-    # Create a windowed mode window and its OpenGL context
-    window = glfw.create_window(640, 480, "Hello World", None, None)
-    if not window:
+
+    def main_loop(self):
+        logger.debug('Init main loop')
+        while not glfw.window_should_close(self.window):
+            glfw.poll_events()
+
+    def cleanup(self):
+        logger.debug('Cleanup')
+        vk.vkDestroyInstance(self.instance, None)
+        glfw.destroy_window(self.window)
         glfw.terminate()
-        return
 
-    # Make the window's context current
-    glfw.make_context_current(window)
+    def create_instance(self):
+        application_info = vk.VkApplicationInfo(
+            sType = vk.VK_STRUCTURE_TYPE_APPLICATION_INFO,
+            pApplicationName = "Pyrengine Test",
+            applicationVersion = vk.VK_MAKE_VERSION(1,0,0),
+            pEngineName = "Pyrengine",
+            engineVersion = vk.VK_MAKE_VERSION(1, 0, 0),
+            apiVersion = vk.VK_API_VERSION
+        )
 
-    # Loop until the user closes the window
-    while not glfw.window_should_close(window):
-        # Render here, e.g. using pyOpenGL
+        extensions = glfw.get_required_instance_extensions()
 
-        # Swap front and back buffers
-        glfw.swap_buffers(window)
+        instance_create_info = vk.VkInstanceCreateInfo(
+            sType = vk.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+            pApplicationInfo = application_info,
+            enabledExtensionCount= len(extensions),
+            ppEnabledExtensionNames=extensions
+        )
 
-        # Poll for and process events
-        glfw.poll_events()
-
-    glfw.terminate()
+        self.instance = vk.vkCreateInstance(instance_create_info, None)
 
 if __name__ == "__main__":
-    main()
+    demo = Demo()
+
+    try:
+        demo.run()
+    except:
+        e = sys.exec_info()[0]
+        print('Error occurred:', e)
